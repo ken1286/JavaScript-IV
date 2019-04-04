0a49c4b7ca4082ba3a8dc81e15fd142ecae794f3
ken1286 <kindle1286@gmail.com>
2019-04-04 11:41:25 -0700

    GameObject and CharacterStats refactored into classes

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,41 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+class GameObject {
+  constructor(attributes) {
+    this.createdAt = attributes.createdAt;
+    this.name = attributes.name;
+    this.dimensions = attributes.dimensions;
+  }
+
+  // Methods
+  destroy() {
+    return `${this.name} was removed from the game.`;
+  }
+}
+
+class CharacterStats extends GameObject {
+  constructor(charAttributes) {
+    super(charAttributes);
+    this.healthPoints = charAttributes.healthPoints;
+  }
+
+  // Methods
+  takeDamage() {
+    return `${this.name} took damage.`;
+  }
+}
+
+function Humanoid(attributes) {
+  CharacterStats.call(this, attributes);
+  this.team = attributes.team;
+  this.weapons = attributes.weapons;
+  this.language = attributes.language;
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function(){
+  return `${this.name} offers a greeting in ${this.language}`;
+}
