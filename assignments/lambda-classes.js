dcf74c9ca5b23a054c73374fe512dd53918192b5
ken1286 <kindle1286@gmail.com>
2019-04-04 12:08:48 -0700

    Instructor class complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -13,3 +13,29 @@ class Person {
     return `Hello my name is ${this.name}, I am from ${this.location}`;
   }
 }
+
+class Instructor extends Person {
+  constructor(attributes) {
+    super(attributes);
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+  }
+
+  // Methods
+  demo(subjectStr) {
+    return `Today we are learning about ${subjectStr}`;
+  }
+
+  grade(studentObj, subjectStr) {
+    return `${studentObj.name} receives a perfect score on ${subjectStr}`;
+  }
+}
+
+class Student extends Person {
+
+}
+
+class ProjectManager extends Instructor {
+
+}
