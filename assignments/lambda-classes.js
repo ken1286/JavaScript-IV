ac6da2a6d2d87e6eb370fcf766c9022c627215cd
ken1286 <kindle1286@gmail.com>
2019-04-04 12:38:34 -0700

    Student class complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,7 +10,7 @@ class Person {
 
   // Methods
   speak() {
-    return `Hello my name is ${this.name}, I am from ${this.location}`;
+    return `Hello my name is ${this.name}, I am from ${this.location}.`;
   }
 }
 
@@ -24,18 +24,63 @@ class Instructor extends Person {
 
   // Methods
   demo(subjectStr) {
-    return `Today we are learning about ${subjectStr}`;
+    return `Today we are learning about ${subjectStr}.`;
   }
 
   grade(studentObj, subjectStr) {
-    return `${studentObj.name} receives a perfect score on ${subjectStr}`;
+    return `${studentObj.name} receives a perfect score on ${subjectStr}.`;
   }
 }
 
 class Student extends Person {
+  constructor(attributes) {
+    super(attributes);
+    this.previousBackground = attributes.previousBackground;
+    this.className = attributes.className;
+    this.favSubjects = attributes.favSubjects;
+  }
 
+  // Methods
+  listsSubjects() {
+    this.favSubjects.forEach(function(element) {
+      console.log(element);
+    });
+  }
+
+  PRAssignment(subject) {
+    return `${this.name} has received a PR for ${subject}.`;
+  }
 }
 
 class ProjectManager extends Instructor {
 
 }
+
+const fred = new Instructor({
+  name: 'Fred',
+  location: 'Bedrock',
+  age: 37,
+  gender: 'male',
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `Don't forget the homies`
+});
+
+const ken = new Student({
+  name: 'Ken',
+  location: 'California',
+  age: 32,
+  gender: 'male',
+  favLanguage: 'JavaScript',
+  specialty: 'back-end',
+  catchPhrase: `See you later`,
+  previousBackground: `Napping`,
+  className: `WEB19`,
+  favSubjects: [`History`, `Coding`, `Video Games`]
+});
+
+console.log(fred.demo(`Spiders`));
+console.log(fred.grade(ken, `Web Dev`));
+ken.listsSubjects();
+console.log(ken.PRAssignment(`Computer Science`));
+console.log(ken.speak());
